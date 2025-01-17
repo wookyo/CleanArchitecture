6ee2c1fa82afe082b0208edd33a06fcfbeb2f1ae
swk0826 <nonstopwk@gmail.com>
2025-01-17 16:23:06 +0900

    [CleanArchitecture] data layer 파일 추가

---
+++ data-layer/src/main/java/com/example/data/db/movie/MovieDatabase.kt
@@ -0,0 +1,19 @@
+package com.example.data.db.movie
+
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.data.model.movie.MovieEntity
+
+abstract class MovieDatabase{}
+
+/** *
+ * [추후 환경설정 작업필요]
+ * Room DB
+ * DB 의 entities 는 MovieEntity 로 설정.
+ *
+ */
+//@Database(entities = [MovieEntity::class], version = 1, exportSchema = false)
+//abstract class MovieDatabase : RoomDatabase() {
+//    abstract fun movieDao(): MovieDao
+//}
---
+++ data-layer/src/main/java/com/example/data/repository/movie/remote/MovieRemoteDataSource.kt
@@ -0,0 +1,22 @@
+package com.example.data.repository.movie.remote
+
+import com.example.data.model.movie.MovieResponse
+import io.reactivex.Single
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Api 호출을 통해 Movie Data 를 가져오기 위한 interface
+ * DataSourceImpl 에서 구현된다.
+ */
+//@DefineComponent(parent = ActivityComponent::class)
+interface MovieRemoteDataSource {
+    fun getSearchMovies(
+        query: String,
+        start: Int = 1
+    ): Single<MovieResponse>
+
+    fun getSearchMoviesFlow(
+        query: String,
+        start: Int = 1
+    ): Flow<MovieResponse>
+}
